956afb31c14e63fe83eed08fda609234541199fd
Adi G <adrian.grecu@rocketmail.com>
2024-01-24 12:53:52 +0200

    latest

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,18 @@ module.exports = {
   e2e: {
     supportFile: 'cypress/support/index.js',
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config) // run old config
+      on('task', {
+        log (message) {
+          console.log(message)
+          return null
+        }
+      })
+      ELECTRON_ENABLE_LOGGING=true
+      // return require('./cypress/plugins/index.js')(on, config) // run old config
     },
+
+    env:{
+      Weather: ''
+    }
   },
 };
--- cypress/e2e/weather.cy.js
+++ cypress/e2e/weather.cy.js
@@ -1,9 +1,15 @@
 
+let temp
+
 describe('Getting current weather.', () => {
     it('GET on weather API and return the current temperature in C for Bucharest', () => {
         cy.getWeather().then((response) => {
-            console.log(response.body.current['temp_c'])
-            // cy.print(response.body.current['temp_c'])
+            // console.log(response.body.current['temp_c'])
+            temp = response.body.current['temp_c']
+            Cypress.env('Weather', temp)
+            cy.log(Cypress.env('Weather'))
+            // cy.log(temp)
         });
+            cy.exec('node mail', {failOnNonZeroExit: false})
     })
 })
